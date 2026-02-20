07d916dc0c35ef9be25bd814ed0c9e72efecba7d
EtheriumBot <ethanrwu@gmail.com>
2026-02-20 13:50:13 -0800

    testing gemini api key

--- app/page.js
+++ app/page.js
@@ -1,3 +1,12 @@
-export default function Page() {
-    return <h1>AI learning tool!!</h1>
-  }
+import { GoogleGenAI } from "@google/genai";
+
+const genAI = new GoogleGenAI("AIzaSyA3BwjQcvPHxrPVelnwV8cCkKDVlr2UiCE");
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+async function runChat() {
+  const prompt = "Write a tagline for a bakery.";
+  const result = await model.generateContent(prompt);
+  console.log(result.response.text());
+}
+
+runChat();
